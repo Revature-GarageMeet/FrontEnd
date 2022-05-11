7157329c8291a863f87562820faad9af47d05890
NovaFlash321 <novaflash321@gmail.com>
2022-05-11 15:19:28 -0400

    color scheme; homepage, login, and register component

--- GarageMeet/src/app/app.module.ts
+++ GarageMeet/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,

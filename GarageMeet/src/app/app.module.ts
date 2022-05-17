21489548b4932b021bb68a36c70c52fd470ce15e
Mo Elsamahi <Elsamahi95@gmail.com>
2022-05-17 14:12:45 -0400

    added front end Registration page and cleaned login

--- GarageMeet/src/app/app.module.ts
+++ GarageMeet/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,

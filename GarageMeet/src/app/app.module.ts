c08e2b64dd1591b926177d8aef29afc78ee1fb31
Arrion7 <102093283+Arrion7@users.noreply.github.com>
2022-05-26 14:12:20 -0400

    Added band component

--- GarageMeet/src/app/app.module.ts
+++ GarageMeet/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { UserprofileComponent } from './userprofile/userprofile.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CommentComponent } from './comment/comment.component';
 import { GrouppageComponent } from './grouppage/grouppage.component';
+import { BandComponent } from './band/band.component';
 
 
 
@@ -33,7 +34,8 @@ import { GrouppageComponent } from './grouppage/grouppage.component';
     PostComponent,
     RegisterComponent,
     NavbarComponent,
-    CommentComponent
+    CommentComponent,
+    BandComponent
 
   ],
   imports: [

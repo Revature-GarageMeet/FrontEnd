05ea20e9cc2c9091a910faeb01d7e305d972c042
Jose Mendoza <mendozaj9486@gmail.com>
2022-05-27 10:21:58 -0500

    Appended comment to post. Capable to submit to database.

--- GarageMeet/src/app/comment/comment.component.ts
+++ GarageMeet/src/app/comment/comment.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommentService } from '../services/comment.service';
+import { Comments, Post } from '../post';
 
 @Component({
   selector: 'app-comment',
@@ -7,16 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentComponent implements OnInit {
 
+  @Input()
+  post!: Post;
+
   textEntry!: string;
 
-  constructor() { }
+  comment: Comments = {
+    commentId: 0,
+    userId: 0,
+    postId: 0,
+    entry: "",
+    likes: 0,
+    dateCreated: new Date()
+  }
+
+  constructor(private api: CommentService) { }
 
   ngOnInit(): void {
   }
 
   submitComment(): void {
-    console.log("submitting comment!");
-    console.log(this.textEntry);
+    this.comment.entry = this.textEntry;
+    this.comment.userId = this.post.userId;
+    this.api.addComment(this.comment, this.post.id).subscribe((res) => {
+      this.textEntry = "";
+    }
+    )
   }
 
 }
